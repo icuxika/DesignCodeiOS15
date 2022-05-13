8327910ccb0efa14ce43c3e9df0d0c4420ba581e
icuxika <icuxika@outlook.com>
2022-05-13 21:53:19 +0800

    Basic view, includes stroke, shadow, shape, color, scheme ...

---
+++ DesignCodeiOS15/Styles.swift
@@ -0,0 +1,38 @@
+//
+//  Styles.swift
+//  DesignCodeiOS15
+//
+//  Created by 🌱🌱 🌱 on 2022/5/13.
+//
+
+import SwiftUI
+
+struct StrokeStyle: ViewModifier {
+    
+    var cornerRadius: CGFloat
+    
+    @Environment(\.colorScheme) var colorScheme
+    
+    func body(content: Content) -> some View {
+        content.overlay(
+            RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+                .stroke(
+                    .linearGradient(
+                        colors: [
+                            .white.opacity(colorScheme == .dark ? 0.6 : 0.3),
+                            .black.opacity(colorScheme == .dark ? 0.3 : 0.1)
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+                .blendMode(.overlay)
+        )
+    }
+}
+
+extension View {
+    func strokeStyle(cornerRadius: CGFloat = 24) -> some View {
+        modifier(StrokeStyle(cornerRadius: cornerRadius))
+    }
+}
