08d88271de4ea99bc5ff9cb1ca769022f804076f
icuxika <icuxika@outlook.com>
2022-05-14 17:50:59 +0800

    Colorful tab bar

--- DesignCodeiOS15/TabBar.swift
+++ DesignCodeiOS15/TabBar.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct TabBar: View {
     
     @State var selectedTab: Tab = .home
+    @State var color: Color = .teal
     
     var body: some View {
         ZStack(alignment: .bottom) {
@@ -30,7 +31,10 @@ struct TabBar: View {
             HStack {
                 ForEach(tabItems) { item in
                     Button {
-                        selectedTab = item.tab
+                        withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
+                            selectedTab = item.tab
+                            color = item.color
+                        }
                     } label: {
                         VStack(spacing: 0) {
                             Image(systemName: item.icon)
@@ -44,12 +48,54 @@ struct TabBar: View {
                         .frame(maxWidth: .infinity)
                     }
                     .foregroundStyle(selectedTab == item.tab ? .primary : .secondary)
+                    .blendMode(selectedTab == item.tab ? .overlay : .normal)
                 }
             }
             .padding(.horizontal, 8)
             .padding(.top, 12)
             .frame(height: 80, alignment: .top)
             .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 32, style: .continuous))
+            .background(
+                HStack {
+                    if selectedTab == .library { Spacer() }
+                    if selectedTab == .explore { Spacer() }
+                    if selectedTab == .notifications {
+                        Spacer()
+                        Spacer()
+                    }
+                    Circle().fill(color).frame(width: 80)
+                    if selectedTab == .home { Spacer() }
+                    if selectedTab == .explore {
+                        Spacer()
+                        Spacer()
+                    }
+                    if selectedTab == .notifications { Spacer() }
+                }
+                    .padding(.horizontal, 8)
+            )
+            .overlay(
+                HStack {
+                    if selectedTab == .library { Spacer() }
+                    if selectedTab == .explore { Spacer() }
+                    if selectedTab == .notifications {
+                        Spacer()
+                        Spacer()
+                    }
+                    Rectangle()
+                        .fill(color)
+                        .frame(width: 24, height: 4)
+                        .cornerRadius(4)
+                        .frame(width: 80)
+                        .frame(maxHeight: .infinity, alignment: .top)
+                    if selectedTab == .home { Spacer() }
+                    if selectedTab == .explore {
+                        Spacer()
+                        Spacer()
+                    }
+                    if selectedTab == .notifications { Spacer() }
+                }
+                    .padding(.horizontal, 8)
+            )
             .strokeStyle()
             .frame(maxHeight: .infinity, alignment: .bottom)
             .ignoresSafeArea()
